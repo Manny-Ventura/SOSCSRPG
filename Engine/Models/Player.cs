f74f11226162426f5b0af01d84ea256b35d579f3
Manuel (Manny) Ventura <70236000+Manny-Ventura@users.noreply.github.com>
2022-11-18 18:35:56 -0500

    Did all character class attributes correctly.

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -11,6 +11,9 @@ namespace Engine.Models
         string Name { get; set; }
         string CharacterClass { get; set; }
         int HitPoints { get; set; }
+        int ExperiencePoints { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
 
 
     }
