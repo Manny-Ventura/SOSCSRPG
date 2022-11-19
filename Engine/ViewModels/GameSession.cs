474e88423c5c14e1483361180f81b98a90195411
Manuel (Manny) Ventura <70236000+Manny-Ventura@users.noreply.github.com>
2022-11-18 19:05:28 -0500

    Edited scope of Player to access attributes.

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Engine.ViewModels
 {
     internal class GameSession
     {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession() 
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+        }
     }
 }
